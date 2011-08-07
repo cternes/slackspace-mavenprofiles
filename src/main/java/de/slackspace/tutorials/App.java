5869a3306d4fb1863071757238782793e3cc2ae0
cte <cte@slackspace.de>
2011-08-07 09:49:53 +0200

    Initial commit

---
+++ src/main/java/de/slackspace/tutorials/App.java
@@ -0,0 +1,9 @@
+package de.slackspace.tutorials;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Nothing here, just a maven example ;)" );
+    }
+}
